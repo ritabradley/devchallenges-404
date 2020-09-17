3e7308f8c5cfa46619a71d7d68066bcbda8ac94f
Restyled.io <commits@restyled.io>
2020-09-17 00:21:41 +0000

    Restyled by prettier

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,21 @@
 module.exports = {
-  future : {
-      // removeDeprecatedGapUtilities: true,
-      // purgeLayersByDefault: true,
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
   },
-  purge : [],
-  theme : {
-    fontFamily : {
-      display : [ 'Inconsolata', 'monospace' ],
-      body : [ 'Space Mono', 'monospace' ],
-      footer : [ 'Montserrat', 'sans-serif' ],
+  purge: [],
+  theme: {
+    fontFamily: {
+      display: ["Inconsolata", "monospace"],
+      body: ["Space Mono", "monospace"],
+      footer: ["Montserrat", "sans-serif"],
     },
-    backgroundColor : (theme) => ({
-      primary : '#333',
+    backgroundColor: (theme) => ({
+      primary: "#333",
     }),
 
-    extend : {},
+    extend: {},
   },
-  variants : {},
-  plugins : [],
+  variants: {},
+  plugins: [],
 };
